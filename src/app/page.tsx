4639769457fd7420b5e601dae580186564f398e5
Kageqt <ealbautista.student@ua.edu.ph>
2025-05-04 10:40:18 +0800

    Added Clerk Auth and Simple Implementation of UploadThing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,38 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
+      {/* <Link href={"about/"}>About Page Link</Link> */}
+      <Images />
     </main>
+  );
+}
+
+function Images() {
+  const urls = [
+    "https://gloosguykl.ufs.sh/f/epljYf3PWgTFtmUXZUpHsCz1NUf2kuxdaXOvJq496nt3YlTW",
+    "https://gloosguykl.ufs.sh/f/epljYf3PWgTFXWrxjVPn18ixTvdmV79bEUD4fMh6LBazcYHA",
+    "https://gloosguykl.ufs.sh/f/epljYf3PWgTFdXijL8EfFRXsImthZrcQNfa2wv9HKUgo83ip",
+  ];
+
+  const images = urls.map((url, index) => ({
+    id: index + 1,
+    url,
+  }));
+
+  return (
+    <div className="flex flex-wrap items-center justify-center gap-6 p-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex w-64 flex-col">
+          <div className="relative aspect-video bg-zinc-900">
+            <img
+              src={image.url}
+              alt={image.id.toString()}
+              className="h-full w-full object-contain object-top"
+            />
+          </div>
+          <div className="text-center">{image.id}</div>
+        </div>
+      ))}
+    </div>
   );
 }
